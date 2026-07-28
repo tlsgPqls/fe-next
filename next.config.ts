61912f1625b3c2aac23b405ff3092792d1e8b591
tlsgPqls <hoddi98@gmail.com>
2026-07-28 11:37:59 +0900

    1st publish

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //그 폴더만 사용 -> standalone 파일 생성됌
+  output: 'standalone',
 };
 
 export default nextConfig;
